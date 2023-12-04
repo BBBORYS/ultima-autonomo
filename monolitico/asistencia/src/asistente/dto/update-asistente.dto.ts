b70e5462c3b1c73cf94cfe4685e9c8f4173cb206
BORYS <e1350920995@live.uleam.edu.ec>
2023-12-04 10:16:45 -0500

    src

--- monolitico/asistencia/src/asistente/dto/update-asistente.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAsistenteDto } from './create-asistente.dto';
-
-export class UpdateAsistenteDto extends PartialType(CreateAsistenteDto) {}
